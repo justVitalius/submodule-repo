b9d792e9537240692603f327a5fb1454ab2950fe
justvitalius <justvitalius@gmail.com>
2017-06-05 22:59:35 +0300

    Add <type> to gets json method

--- jsonable.js
+++ jsonable.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
-module.exports = function jsonable(app, flow, state) {
+module.exports = function jsonable(app, flow, state, type='rq') {
   return require(
-    path.resolve(__dirname, 'schemas', app, flow, state)
+    path.resolve(__dirname, 'schemas', app, flow, state, type)
   );
 }
